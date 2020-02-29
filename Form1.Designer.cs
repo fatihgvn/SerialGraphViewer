76c39f39dffaf4abb95feb938c59c475eb45a3d0
Fatih Güven <31927632+fatihgvn@users.noreply.github.com>
2020-02-29 21:42:16 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace SerialGraphViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.comlist = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.autoScroll = new System.Windows.Forms.CheckBox();
+            this.displaySinus = new System.Windows.Forms.CheckBox();
+            this.displayLine = new System.Windows.Forms.CheckBox();
+            this.displaySquare = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sempler = new System.Windows.Forms.CheckBox();
+            this.saveImage = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.ReadTimeout = 500;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comlist,
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(882, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // comlist
+            // 
+            this.comlist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comlist.Name = "comlist";
+            this.comlist.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::SerialGraphViewer.Properties.Resources.open;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::SerialGraphViewer.Properties.Resources.refresh;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(882, 492);
+            this.splitContainer1.SplitterDistance = 340;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Sinüs";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Çizgi";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series6.Legend = "Legend1";
+            series6.Name = "Kare";
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(882, 340);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(731, 148);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.saveImage);
+            this.splitContainer2.Panel2.Controls.Add(this.sempler);
+            this.splitContainer2.Panel2.Controls.Add(this.button1);
+            this.splitContainer2.Panel2.Controls.Add(this.displaySquare);
+            this.splitContainer2.Panel2.Controls.Add(this.displayLine);
+            this.splitContainer2.Panel2.Controls.Add(this.displaySinus);
+            this.splitContainer2.Panel2.Controls.Add(this.autoScroll);
+            this.splitContainer2.Size = new System.Drawing.Size(882, 148);
+            this.splitContainer2.SplitterDistance = 731;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // autoScroll
+            // 
+            this.autoScroll.AutoSize = true;
+            this.autoScroll.Checked = true;
+            this.autoScroll.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.autoScroll.Location = new System.Drawing.Point(3, 3);
+            this.autoScroll.Name = "autoScroll";
+            this.autoScroll.Size = new System.Drawing.Size(77, 17);
+            this.autoScroll.TabIndex = 0;
+            this.autoScroll.Text = "Auto Scroll";
+            this.autoScroll.UseVisualStyleBackColor = true;
+            // 
+            // displaySinus
+            // 
+            this.displaySinus.AutoSize = true;
+            this.displaySinus.Location = new System.Drawing.Point(3, 26);
+            this.displaySinus.Name = "displaySinus";
+            this.displaySinus.Size = new System.Drawing.Size(136, 17);
+            this.displaySinus.TabIndex = 1;
+            this.displaySinus.Text = "Sinüzoidal Göster/Gizle";
+            this.displaySinus.UseVisualStyleBackColor = true;
+            this.displaySinus.CheckedChanged += new System.EventHandler(this.displaySinus_CheckedChanged);
+            // 
+            // displayLine
+            // 
+            this.displayLine.AutoSize = true;
+            this.displayLine.Checked = true;
+            this.displayLine.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.displayLine.Location = new System.Drawing.Point(3, 49);
+            this.displayLine.Name = "displayLine";
+            this.displayLine.Size = new System.Drawing.Size(110, 17);
+            this.displayLine.TabIndex = 2;
+            this.displayLine.Text = "Çizgi Göster/Gizle";
+            this.displayLine.UseVisualStyleBackColor = true;
+            this.displayLine.CheckedChanged += new System.EventHandler(this.displayLine_CheckedChanged);
+            // 
+            // displaySquare
+            // 
+            this.displaySquare.AutoSize = true;
+            this.displaySquare.Location = new System.Drawing.Point(3, 72);
+            this.displaySquare.Name = "displaySquare";
+            this.displaySquare.Size = new System.Drawing.Size(136, 17);
+            this.displaySquare.TabIndex = 3;
+            this.displaySquare.Text = "Karedalga Göster/Gizle";
+            this.displaySquare.UseVisualStyleBackColor = true;
+            this.displaySquare.CheckedChanged += new System.EventHandler(this.displaySquare_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sempler
+            // 
+            this.sempler.AutoSize = true;
+            this.sempler.Location = new System.Drawing.Point(3, 95);
+            this.sempler.Name = "sempler";
+            this.sempler.Size = new System.Drawing.Size(107, 17);
+            this.sempler.TabIndex = 5;
+            this.sempler.Text = "Örnekleme Modu";
+            this.sempler.UseVisualStyleBackColor = true;
+            this.sempler.CheckedChanged += new System.EventHandler(this.sempler_CheckedChanged);
+            // 
+            // saveImage
+            // 
+            this.saveImage.Location = new System.Drawing.Point(74, 118);
+            this.saveImage.Name = "saveImage";
+            this.saveImage.Size = new System.Drawing.Size(65, 23);
+            this.saveImage.TabIndex = 6;
+            this.saveImage.Text = "Kaydet";
+            this.saveImage.UseVisualStyleBackColor = true;
+            this.saveImage.Click += new System.EventHandler(this.saveImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 517);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SerialGraphViewer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox comlist;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.CheckBox autoScroll;
+        private System.Windows.Forms.CheckBox displaySquare;
+        private System.Windows.Forms.CheckBox displayLine;
+        private System.Windows.Forms.CheckBox displaySinus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox sempler;
+        private System.Windows.Forms.Button saveImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SerialGraphViewer
+{
+    public partial class Form1 : Form
+    {
+        bool stopDraw = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            comlist.Items.Clear();
+            comlist.Items.AddRange(SerialPort.GetPortNames());
+            comlist.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comlist.Items.AddRange(SerialPort.GetPortNames());
+            comlist.SelectedIndex = 0;
+
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+
+            chart1.Series[0].XValueType = ChartValueType.Auto;
+            chart1.Series[1].XValueType = ChartValueType.Auto;
+            chart1.Series[2].XValueType = ChartValueType.Auto;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "";
+
+            chart1.Series[0].Points.AddY(0);
+            chart1.Series[1].Points.AddY(0);
+            chart1.Series[2].Points.AddY(0);
+
+            chart1.Series[0].Enabled = false;
+            chart1.Series[2].Enabled = false;
+
+            chart1.MouseWheel += Chart1_MouseWheel;
+        }
+
+        private void Chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            var xAxis = chart.ChartAreas[0].AxisX;
+            var yAxis = chart.ChartAreas[0].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Scrolled down.
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+                else if (e.Delta > 0) // Scrolled up.
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    toolStripButton1.Image = Properties.Resources.close;
+
+                    serialPort1.PortName = comlist.Text;
+                    serialPort1.Open();
+                }
+                else
+                {
+                    toolStripButton1.Image = Properties.Resources.open;
+                    this.Invoke(() =>
+                    {
+                        serialPort1.Close();
+                    });
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (stopDraw) return;
+
+                SerialPort sp = (SerialPort)sender;
+                //string indata = sp.ReadExisting();
+                string indata = sp.ReadLine();
+                double doubleData;
+                Double.TryParse(indata, out doubleData);
+
+                var match = Regex.Match(indata, @"^\-?\d+[\n\r]+?$", RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    try
+                    {
+                        chart1.Invoke(() =>
+                        {
+                            chart1.Series[0].Points.AddY(doubleData);
+                            chart1.Series[1].Points.AddY(doubleData);
+                            chart1.Series[2].Points.AddY(doubleData);
+
+                            if(sempler.Checked && chart1.Series[0].Points.Count > 301)
+                            {
+                                chart1.Series[0].Points.RemoveAt(0);
+                                chart1.Series[1].Points.RemoveAt(0);
+                                chart1.Series[2].Points.RemoveAt(0);
+
+                                chart1.ChartAreas[0].RecalculateAxesScale();
+                            }
+                                
+                        });
+
+                    }
+                    catch (Exception ex)
+                    {
+                        richTextBox1.Invoke(() => { richTextBox1.AppendText(ex.Message.ToString() + Environment.NewLine); });
+                    }
+
+                }
+                richTextBox1.Invoke(() => { richTextBox1.AppendText(indata); });
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (autoScroll.Checked)
+            {
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+            }
+        }
+
+        private void displaySinus_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[0].Enabled = displaySinus.Checked;
+        }
+
+        private void displayLine_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[1].Enabled = displayLine.Checked;
+        }
+
+        private void displaySquare_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[2].Enabled = displaySquare.Checked;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    this.Invoke(() =>
+                    {
+                        serialPort1.Close();
+                    });
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+
+            chart1.Series[0].Points.AddY(0);
+            chart1.Series[1].Points.AddY(0);
+            chart1.Series[2].Points.AddY(0);
+        }
+
+        private void sempler_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sempler.Checked)
+            {
+                chart1.ChartAreas[0].AxisX.Maximum = 300;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisX.Maximum = Double.NaN;
+            }
+        }
+
+        private void saveImage_Click(object sender, EventArgs e)
+        {
+            stopDraw = true;
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Resim|*.png";
+            save.FileName = "Chart.png";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                chart1.SaveImage(save.FileName, ChartImageFormat.Png);
+            }
+            stopDraw = false;
+        }
+    }
+
+    public static class ControlExtensions
+    {
+        public static void Invoke(this Control control, Action action)
+        {
+            if (control.InvokeRequired) control.Invoke(new MethodInvoker(action), null);
+            else action.Invoke();
+        }
+    }
+}
